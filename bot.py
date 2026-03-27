5d41fc1a6d83478e6f01a3ceb9c3dde8ebe68d7a
ccw0rk3r <155923938+ccw0rk3r@users.noreply.github.com>
2026-03-27 17:24:42 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -332,6 +332,7 @@ async def cancel_broadcast(callback: CallbackQuery, state: FSMContext):
     await callback.answer()
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
